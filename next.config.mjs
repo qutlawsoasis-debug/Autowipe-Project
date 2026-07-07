d8308c55ac3a416f7b0154aa6ec1baa1422cba98
qutlawsoasis-debug <qutlawsoasis@gmail.com>
2026-07-08 00:07:24 +0200

    Configure static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://127.0.0.1:5001/api/:path*',
-      },
-      {
-        source: '/media',
-        destination: 'http://127.0.0.1:5001/media',
-      },
-    ]
   },
 }
 
